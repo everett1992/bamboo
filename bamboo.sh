563caa24e0325ec96fd4d75ff48f82e563ef90a7
Caleb Everett <everett.caleb@gmail.com>
2014-02-25 00:44:42 -0500

    Replace placeholder text using sed

--- bamboo.sh
+++ bamboo.sh
@@ -1,31 +1,52 @@
 #!/bin/bash
 
+#:: Define some helpfull functions
+
 debug() { echo $@; }
 error() { echo $script_name: $@ 1>&2; }
 
+#:: Get command line arguments, and setup scripts from the shell state.
+
 script_name=$(basename $0)
-
-lsym='-{'
-rsym='}-'
-valid_chars='\w'
-
-# Global vars
-first_name="Caleb"
-last_name="Everett"
-full_name="$first_name $last_name"
-
-# Auto vars
 scaffold="$1"
 project_name="$2"
-project_path="$PWD/"
-
-# TODO: create system and user scaffold folders.
+project_path="$PWD"
 scaffolds_dir=~/workspace/bamboo/scaffolds
 
-echo "scaffold_dir: $scaffolds_dir"
-echo "scaffold:     $scaffold"
-echo "project_name: $project_name"
-echo "project_path: $project_path"
+# bamboo placeholders are surrouned by _{ and }_
+# and must be made of only word charecters
+lsym='_{'
+rsym='}_'
+valid_chars='\w'
+
+
+#:: Setup bamboo placeholder replacement values
+
+declare -A replacements
+
+# Global replacements
+replacements=(
+  ["first_name"]="Caleb"
+  ["last_name"]="Everett"
+
+  ["scaffold"]="$scaffold"
+  ["project_name"]="$project_name"
+  ["project_path"]="$project_path"
+
+  ["scaffolds_dir"]="$scaffolds_dir"
+)
+replacements["full_name"]="${replacements["first_name"]} ${replacements["last_name"]}"
+
+
+print_replacements() {
+  for placeholder in "${!replacements[@]}"; do
+    replacemnet="${replacements["$placeholder"]}"
+    echo "$placeholder:	$replacemnet"
+  done | column -t -s '	'
+}
+
+
+#:: Check that scaffold exists
 
 if [ -d "$scaffolds_dir/$scaffold" ]; then
   debug "Found scaffold $scaffold in $scaffolds_dir"
@@ -33,3 +54,30 @@ else
   error "Couldn't find scaffold $scaffold in $scaffolds_dir"
   exit 1
 fi
+
+
+#:: Begin replacing placeholders with values.
+
+# I'd like to pass the dictionary of replacements to
+# this as a function, but I don't know how to in bash
+build_sed() {
+  sed_expressions=""
+
+  for placeholder in "${!replacements[@]}"; do
+    # escape any charecters that might give sed trouble
+    trouble_chars="\/"
+    replacement="$(echo "${replacements["$placeholder"]}" | sed -s "s/\([$trouble_chars]\)/\\\\\1/g" -)"
+    sed_expressions+="s/${lsym}${placeholder}${rsym}/${replacement}/g; "
+  done
+
+  echo "$sed_expressions"
+}
+
+# List of all scaffold subfiles, filenames are relative to the scaffold's dir.
+files=$(cd $scaffolds_dir/$scaffold; find -mindepth 1)
+
+sed_s="$(build_sed)"
+
+for file in $files; do
+  sed -s "$sed_s" "$scaffolds_dir/$scaffold/$file"
+done
---
+++ scaffolds/subdir/note.md
@@ -0,0 +1,4 @@
+# _{project_name}_
+_{full_name}_
+
+this project exists to test scaffolds with multiple directories
